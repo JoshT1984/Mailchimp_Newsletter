b91b001573753e14505705fa35a1fbc4499daa58
JoshT1984 <warlord84502@yahoo.com>
2022-12-28 22:26:57 -0600

    First Commit

---
+++ public/js/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const client = require("@mailchimp/mailchimp_marketing");
+const chimpAPI = "";
+const apiServer = "";
+const chimpListID = "";
+
+client.setConfig({
+  apiKey: chimpAPI,
+  server: apiServer,
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  "/images",
+  express.static(path.join(__dirname + "../../../public/images"))
+);
+
+app.use("/css", express.static(path.join(__dirname + "../../../public/css")));
+
+app.use("/js", express.static(path.join(__dirname + "../../../public/js")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "../../../signup.html"));
+});
+
+app.post("/", (req, res) => {
+  const firstName = req.body.first;
+  const lastName = req.body.last;
+  const email = req.body.email;
+  const listID = chimpListID;
+
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+  };
+
+  const run = async () => {
+    const response = await client.lists.addListMember(listID, {
+      email_address: subscribingUser.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: subscribingUser.firstName,
+        LNAME: subscribingUser.lastName,
+      },
+    });
+    res.sendFile(path.join(__dirname + "../../../success.html"));
+  };
+  run().catch((e) => {
+    res.sendFile(path.join(__dirname + "../../../failure.html"));
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port 3000");
+});
